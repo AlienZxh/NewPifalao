a5a28175d2d7960b78b25830f4d446c01283b018
AlbertZ <1047434395@qq.com>
2017-06-26 08:52:37 +0800

    周末备份4

--- app/src/main/java/com/j1j2/pifalao/Constants.kt
+++ app/src/main/java/com/j1j2/pifalao/Constants.kt
@@ -4,6 +4,6 @@ package com.j1j2.pifalao
  * Created by zxh19 on 2017/6/25.
  */
 object Constants {
-    val WEIXIN_APP_ID = "wxaaf65494c086b0d3"
-    val BUGLY_ID = "1103829290"
+    const val WEIXIN_APP_ID = "wxaaf65494c086b0d3"
+    const val BUGLY_ID = "1103829290"
 }
